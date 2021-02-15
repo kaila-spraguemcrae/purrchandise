183bb98df8334fd9ccc41680e10187e5a9da4cee
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-15 16:50:32 -0500

    pass test 2
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- src/__tests__/reducers/product-list-reducer.test.js
+++ src/__tests__/reducers/product-list-reducer.test.js
@@ -2,11 +2,42 @@ import productListReducer from '../../reducers/product-list-reducer'
 
 describe('productListReducer', () => {
 
-  describe('productListReducer', ()=> {
+  let action;
+  const productData = {
+    name: "Coffee Mug",
+    category: "Drinkware",
+    description: "a vessel for fluids",
+    price: "10.00",
+    quantity: 4,
+    id: 1
+  }
 
-    test('Should return default state if there is no action type passed into the reducer', ()=>{
-      expect(productListReducer({},{ type: null})).toEqual({});
-    });
+  test('Should return default state if there is no action type passed into the reducer', ()=>{
+    expect(productListReducer({},{ type: null})).toEqual({});
+  });
+  
+
+  test('Adding a product to our masterProductList', ()=>{
+    const {name, category, description, quantity, price, id} = productData;
+    action = {
+      type: 'ADD_PRODUCT',
+      name: name,
+      category: category,
+      description: description,
+      price: price,
+      quantity: quantity,
+      id: id
+    }
+    expect(productListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        category: category,
+        description: description,
+        price: price,
+        quantity: quantity,
+        id: id
+      }
+    })
   });
 
 });
--- src/reducers/product-list-reducer.js
+++ src/reducers/product-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const {name, category, description, price, quantity, id } = action;
+  switch(action.type){
+    case 'ADD_PRODUCT':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          category: category,
+          description: description,
+          price: price,
+          quantity: quantity,
+          id: id
+        }
+      });
+      default:
+      return state;
+  }
 };
