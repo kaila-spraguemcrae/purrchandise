1bb2fb5656526207bce901d1a32b670ed81a44c2
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-21 10:24:44 -0500

    update productList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import Product from './Product';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 function ProductList(props){
 
-  return (
-    <>
-      {Object.values(props.productList).map((product)=>
-      <div>
-        <Product 
-        whenProductClicked ={props.onProductSelection}
-        name={product.name}
-        category={product.category}
-        description={product.description}
-        price={product.price}
-        quantity={product.quantity}
-        id={product.id}
-        key={product.id}  />
-      </div>
-      )}
-    </>
-  );
+  useFirestoreConnect([
+    { collection: 'products' }
+  ]);
+
+  const products = useSelector(state => state.firestore.ordered.products)
+
+  if (isLoaded(products)){
+    return (
+      <>
+        { products.map((product)=> {
+          return <Product 
+            whenProductClicked ={props.onProductSelection}
+            name={product.name}
+            category={product.category}
+            description={product.description}
+            price={product.price}
+            quantity={product.quantity}
+            id={product.id}
+            key={product.id}  />
+        })}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h3>Loading...</h3>
+      </>
+    )
+  }
 }
 
 ProductList.propTypes = {
-  productList: PropTypes.object,
   onProductSelected: PropTypes.func
 };
 
