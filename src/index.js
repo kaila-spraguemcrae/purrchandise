7bbba90dc2c01a51f3c1b099047262c486cf3e84
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-21 09:54:14 -0500

    add firestoreReducer

--- src/index.js
+++ src/index.js
@@ -3,27 +3,29 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
-import reducer from './reducers/product-list-reducer';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { ceateFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
 
 const store = createStore(rootReducer);
-//const store = createStore(reducer);
 
-store.subscribe(()=>
-  console.log(store.getState())
-);
+const rrfProps = {
+  firebase, 
+  config: {
+      userProfile: "users"
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
   <Provider store = {store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>  
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import formVisibleReducer from './form-visible-reducer';
 import productListReducer from './product-list-reducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  masterProductList: productListReducer
+  masterProductList: productListReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
